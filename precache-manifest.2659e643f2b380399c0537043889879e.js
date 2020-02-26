84107735b6f07d19794ae7993205606ab0a75184
Anna Matieva <anna_matieva@epam.com>
2020-02-26 15:15:53 +0300

    Updates

---
+++ precache-manifest.2659e643f2b380399c0537043889879e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e3eec7ec205a271f37987b685c2b5b1",
+    "url": "/animate/index.html"
+  },
+  {
+    "revision": "430e9e07cadfb145fc32",
+    "url": "/animate/static/css/main.9d1bcd4c.chunk.css"
+  },
+  {
+    "revision": "81a41676ac31cb542a2b",
+    "url": "/animate/static/js/2.51d4ba6c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/animate/static/js/2.51d4ba6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "430e9e07cadfb145fc32",
+    "url": "/animate/static/js/main.eb4d90f0.chunk.js"
+  },
+  {
+    "revision": "b59f66c7e5a20cb51f99",
+    "url": "/animate/static/js/runtime-main.78f60783.js"
+  }
+]);
